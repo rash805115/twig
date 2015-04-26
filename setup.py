30e6aafbf1b8c15b8bbf27a032c3122b6dd2cf3e
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2015-04-26 11:30:46 -0400

    Python - setup.py updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+        name = "Twig",
+        version = 0.0,
+        url = "https://github.com/rash805115/twig",
+        author = "Rahul Chaudhary",
+        author_email = "rahul300chaudhary400@gmail.com",
+        description = "Desktop software for bookeeping.",
+        install_requires = ["PySide == 1.2.2", "pybookeeping == 0.0.0"]
+)
