c4d4913d3e7385e6492c43d28423d1b6bd2d7958
rash805115 <rahul300chaudhary400@gmail.com>
2015-05-05 17:19:26 -0400

    Setup.py update
    The setup.py is now using cxfreeze to package and distribute the
    application.

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
-import setuptools
+import sys
+import cx_Freeze
 
-setuptools.setup(
-        name = "Twig",
-        version = 0.0,
-        url = "https://github.com/rash805115/twig",
-        author = "Rahul Chaudhary",
-        author_email = "rahul300chaudhary400@gmail.com",
+sys.path.append("twig")
+
+base = None
+if sys.platform == "win32":
+	base = "Win32GUI"
+
+cx_Freeze.setup(
+	name = "Twig",
+        version = "0.0",
         description = "Desktop software for bookeeping.",
-        install_requires = ["PySide == 1.2.2", "pybookeeping == 0.0.0"]
+	executables = [cx_Freeze.Executable("twig/service/start.py", base = base, targetName = "twig.exe")]
 )
+
+# import setuptools
+# 
+# setuptools.setup(
+#         name = "Twig",
+#         version = 0.0,
+#         url = "https://github.com/rash805115/twig",
+#         author = "Rahul Chaudhary",
+#         author_email = "rahul300chaudhary400@gmail.com",
+#         description = "Desktop software for bookeeping.",
+#         install_requires = ["PySide == 1.2.2", "pybookeeping == 0.0.0"]
+# )
